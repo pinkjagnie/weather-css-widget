c3b63d0b58c30805a1e77b5aff17674169863aa7
pinkjagnie <olga.krzysztofik@gmail.com>
2024-07-31 10:20:10 +0200

    Styled InfoBadge

--- src/app/components/InfoBadge.js
+++ src/app/components/InfoBadge.js
@@ -1,7 +1,13 @@
 const InfoBadge = () => {
   return (
-    <div className="w-[100%] h-auto badge bg-base-100 gap-2 p-2 rounded-md">
-      <div>Przewidywane przelotne opady w: czwartek po południu</div>
+    <div className="w-[100%] h-auto badge flex flex-col bg-base-100 gap-2 p-2 rounded-md">
+      <p className="uppercase font-semibold text-sm self-start pl-2 text-gray-600">
+        Przegląd pogody
+      </p>
+      <div className="-mx-4 w-[calc(100%+1rem)] border-b-2 border-gray-200"></div>
+      <div className="pl-2 pt-2">
+        Przewidywane przelotne opady w: czwartek po południu
+      </div>
     </div>
   );
 };
