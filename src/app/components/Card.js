93b733c9207924c27c957917087367c458126fda
pinkjagnie <olga.krzysztofik@gmail.com>
2024-07-31 08:48:07 +0200

    Styled top of main widget

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -2,7 +2,12 @@ import { AiOutlineSun } from "react-icons/ai";
 
 const Card = () => {
   return (
-    <div className="card lg:card-side bg-base-100 border-2 rounded-md">
+    <div className="card lg:card-side bg-base-100 border-2 rounded-md p-4">
+      <div className="flex justify-between items-center pb-2 font-semibold">
+        <p className="uppercase text-gray-600">Obecna pogoda</p>
+        <p>13:39</p>
+      </div>
+      <div className="-mx-4 w-[calc(100%+2rem)] border-b-2 border-gray-200"></div>
       <figure>
         <AiOutlineSun size={100} className="text-amber-500" />
       </figure>
