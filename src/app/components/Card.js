498a762dffba866df1d40a3a95bcf956d58119c1
pinkjagnie <olga.krzysztofik@gmail.com>
2024-07-31 10:00:18 +0200

    Styled 'sunny part' on main widget for small devices

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,4 +1,5 @@
 import { AiOutlineSun } from "react-icons/ai";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Card = () => {
   return (
@@ -8,15 +9,40 @@ const Card = () => {
         <p>13:39</p>
       </div>
       <div className="-mx-4 w-[calc(100%+2rem)] border-b-2 border-gray-200"></div>
-      <figure>
-        <AiOutlineSun size={100} className="text-amber-500" />
-      </figure>
+
+      {/* TOP / LEFT INFOS WITH SUN */}
+
+      <div className="flex flex-col items-start p-4">
+        <div className="flex items-start space-x-4">
+          <AiOutlineSun className="text-8xl text-amber-500" />
+          <div>
+            <div className="flex items-start font-bold">
+              <p className="text-6xl leading-none">73</p>
+              <div className="flex flex-col items-start text-2xl leading-none">
+                <span className="leading-none">o</span>
+                <span className="font-semibold text-gray-600">F</span>
+              </div>
+            </div>
+            <div className="text-lg">
+              <p>RealFeel</p>
+              <span>81°</span>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 font-semibold">
+          <p className="text-xl">Słonecznie</p>
+          <div className="flex items-center text-sm uppercase cursor-pointer">
+            <span>Więcej szczegółów</span>
+            <MdKeyboardArrowRight className="text-xl" />
+          </div>
+        </div>
+      </div>
+
+      {/* RIGHT INFOS */}
+
       <div className="card-body">
         <h2 className="card-title">New album is released!</h2>
         <p>Click the button to listen on Spotiwhy app.</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Listen</button>
-        </div>
       </div>
     </div>
   );
