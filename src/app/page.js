6bac293f5ef69ce48ccb393f3aa5c371550e4fe9
pinkjagnie <olga.krzysztofik@gmail.com>
2024-07-31 22:31:43 +0200

    Added main text-color

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Container from "./components/Conteiner";
 
 export default function Home() {
   return (
-    <main className="min-h-screen py-24 px-6">
+    <main className="min-h-screen py-24 px-6 text-stone-950">
       <Container />
     </main>
   );
